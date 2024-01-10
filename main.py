3a31ad0d5b36dae856660b3e208f7b6085065cde
The-One-Program <fyre@LAPTOP-P3VBONFU>
2024-01-10 22:55:26 +0530

    modified config.py for 'cuda'

--- main.py
+++ main.py
@@ -24,11 +24,12 @@ STEP5: call the fit function to fit the Wavelet to the Graph.
 END
 
 """
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = FilterConv(in_channels = IN_CHANNELS, out_channels = OUT_CHANNELS)
-model.to(device = DEVICE)
+model.to(device)
 
-data = torch.load(DATA_PATH)
+data = torch.load(DATA_PATH).to(device)
 # ic(data.shape, x[0].shape)
 x = torch.split(data, min(BATCH_SIZE*500, data.size(0)), 0)
 
@@ -39,11 +40,14 @@ x = torch.split(data, min(BATCH_SIZE*500, data.size(0)), 0)
 # ic(model(x1[0]).shape)
 
 # Initializing
-awt = DWT1d(filter_model = model)
+awt = DWT1d(filter_model = model, device=device).to(device=device)
 
 # Training
-awt.fit(X=x[0],batch_size = BATCH_SIZE, num_epochs = NUM_EPOCHS, lr= LR)
+awt.fit(X=x[0],batch_size = BATCH_SIZE, num_epochs = NUM_EPOCHS, lr= LR).to(device)
 name = f"models/{awt.__module__}__BATCH-{BATCH_SIZE}__EPOCH-{NUM_EPOCHS}__DATA-{DATA_NAME}__FILTER-{OUT_CHANNELS}__TIME-{time.time()}.pth"
 # print(name)
 
 torch.save(awt, name)
+
+# if __name__ == "__main__":
+#     pass
